33d7def94fe0cf9e1e54cad37c2204e67f1f26c1
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2024-07-04 21:41:57 +0200

    Proper navigation with bread crump fixes

--- src/components/menu/navigation.tsx
+++ src/components/menu/navigation.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { makeStyles } from '@fluentui/react-components';
 import {
   Board20Filled,
@@ -17,7 +19,8 @@ import {
   NavSectionHeader,
 } from '@fluentui/react-nav-preview';
 import Link from 'next/link';
-import React from 'react';
+import { usePathname } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
 
 import HamburgerMenu from './hamburger-menu';
 
@@ -52,21 +55,50 @@ type NavigationProps = {
 
 const Navigation = ({ isOpen, toggleHamburgerMenu }: NavigationProps) => {
   const styles = useStyles();
+  const pathname = usePathname();
+
+  const [selectedValue, setSelectedValue] = useState('1');
+
+  useEffect(() => {
+    switch (pathname) {
+      case '/':
+        setSelectedValue('1');
+        break;
+      case '/about':
+        setSelectedValue('2');
+        break;
+      default:
+    }
+  }, [pathname]);
 
   return (
-    <NavDrawer open={isOpen} type='inline' className={styles.root}>
+    <NavDrawer
+      open={isOpen}
+      type='inline'
+      className={styles.root}
+      defaultValue={selectedValue}
+      selectedValue={selectedValue}
+    >
       <NavDrawerHeader className={styles.hamburger}>
         <HamburgerMenu toggleHamburgerMenu={toggleHamburgerMenu} />
       </NavDrawerHeader>
 
       <NavDrawerBody className={styles.navigation}>
         <Link href='/'>
-          <NavItem icon={<DashboardIcons />} value='1'>
+          <NavItem
+            icon={<DashboardIcons />}
+            value='1'
+            onClick={() => setSelectedValue('1')}
+          >
             Dashboard
           </NavItem>
         </Link>
         <Link href='/about'>
-          <NavItem icon={<AboutIcons />} value='2'>
+          <NavItem
+            icon={<AboutIcons />}
+            value='2'
+            onClick={() => setSelectedValue('2')}
+          >
             About
           </NavItem>
         </Link>
@@ -75,7 +107,11 @@ const Navigation = ({ isOpen, toggleHamburgerMenu }: NavigationProps) => {
 
         <NavSectionHeader>Content</NavSectionHeader>
         <Link href='/decks'>
-          <NavItem icon={<DecksIcons />} value='3'>
+          <NavItem
+            icon={<DecksIcons />}
+            value='3'
+            onClick={() => setSelectedValue('3')}
+          >
             Decks
           </NavItem>
         </Link>
