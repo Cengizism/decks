19469e5dcac830782b26c514148146dfc5c19cac
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2024-07-06 15:06:21 +0200

    Improved styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Caption1, Subtitle2, Title2 } from '@fluentui/react-components';
+import { Body2, Caption1, LargeTitle } from '@fluentui/react-components';
 import { makeStyles, tokens } from '@fluentui/react-components';
 import { format } from 'date-fns';
 import React from 'react';
@@ -9,8 +9,8 @@ const useStyles = makeStyles({
   header: {
     display: 'flex',
     flexDirection: 'column',
-    gap: '6px',
-    margin: '20px 6px 36px 6px',
+    gap: '2px',
+    margin: '36px 6px',
   },
   subTitle: {
     color: tokens.colorNeutralForeground4,
@@ -30,10 +30,8 @@ const Header: React.FC<HeaderProps> = ({ title, subTitle, date }) => {
 
   return (
     <header className={styles.header}>
-      <Title2 block>{title}</Title2>
-      {subTitle && (
-        <Subtitle2 className={styles.subTitle}>{subTitle}</Subtitle2>
-      )}
+      <LargeTitle block>{title}</LargeTitle>
+      {subTitle && <Body2 className={styles.subTitle}>{subTitle}</Body2>}
       {formattedDate && (
         <Caption1>
           <strong>Last modified:</strong>{' '}
