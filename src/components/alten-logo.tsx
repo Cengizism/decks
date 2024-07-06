c8bc2d7f7d50f281a36381fcb50b16653eb88e3f
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2024-07-06 13:48:04 +0200

    Improvements for Logo component

--- src/components/alten-logo.tsx
+++ src/components/alten-logo.tsx
@@ -2,14 +2,30 @@
 
 import { TITLE } from '@/lib/constants';
 import { Title1 } from '@fluentui/react-components';
+import { makeStyles } from '@fluentui/react-components';
 import Image from 'next/image';
+import Link from 'next/link';
+import Logo from 'public/alten-logo.svg';
 import React from 'react';
 
-const AltenLogo: React.FC = () => (
-  <hgroup>
-    <Image src='/alten-logo.svg' alt='Alten Logo' width={21} height={32} />
-    <Title1>{TITLE}</Title1>
-  </hgroup>
-);
+const useStyles = makeStyles({
+  logo: {
+    textDecoration: 'none',
+    color: 'inherit',
+  },
+});
+
+const AltenLogo: React.FC = () => {
+  const styles = useStyles();
+
+  return (
+    <hgroup>
+      <Image src={Logo} alt='Alten Logo' priority />
+      <Link className={styles.logo} href='/'>
+        <Title1>{TITLE}</Title1>
+      </Link>
+    </hgroup>
+  );
+};
 
 export default AltenLogo;
