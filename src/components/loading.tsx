7b0491fb82c8dc6336b2d35a5e0305c6dd34b1c0
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2024-07-06 07:27:46 +0200

    Added loading pages for pages to improve UX

---
+++ src/components/loading.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import styles from '../styles/loading.module.css';
+
+interface LoadingProps {
+  children?: React.ReactNode;
+  message?: string;
+}
+
+const Loading: React.FC<LoadingProps> = ({
+  children,
+  message = 'Loading...',
+}) => {
+  return (
+    <div className={styles.loadingContainer}>
+      <div className={styles.spinner}></div>
+      <p>{message}</p>
+      {children && <div className={styles.childrenContainer}>{children}</div>}
+    </div>
+  );
+};
+
+export default Loading;
