c429e4b6273e5bbefd76fb825aedd419380493ea
Cengiz Ulusoy <Cengizism@users.noreply.github.com>
2024-07-18 07:01:13 +0200

    Organise and improve metadata icons and manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,31 @@
+import { TITLE, DESCRIPTION } from '@/constants'
+import { MetadataRoute } from 'next'
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: TITLE,
+    short_name: TITLE,
+    description: DESCRIPTION,
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#fff',
+    theme_color: '#fff',
+    icons: [
+      {
+        src: '/favicon.ico',
+        sizes: 'any',
+        type: 'image/x-icon',
+      },
+      {
+        src: '/icon.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+      },
+      {
+        src: '/apple-icon.png',
+        sizes: 'any',
+        type: 'image/png',
+      },
+    ],
+  }
+}
